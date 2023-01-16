e3a72842386d7af921f01705f004426ab116528b
SantiRuiz7 <ruizsantiagodavid@gmail.com>
2023-01-16 11:24:22 -0300

    Proyecto final

---
+++ js/funciones.js
@@ -0,0 +1,27 @@
+
+document.getElementById('boton1').addEventListener('click', function () {
+        var x = document.getElementById("demo1");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      });
+
+ document.getElementById('boton2').addEventListener('click', function () {
+        var x = document.getElementById("demo2");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      });
+
+ document.getElementById('boton3').addEventListener('click', function () {
+        var x = document.getElementById("demo3");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      });
